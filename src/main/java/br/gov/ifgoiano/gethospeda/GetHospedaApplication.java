da514960a22abab26ffb3150fb506af89088e74a
josevictoraraujorojas <kaneki1212112121@gmail.com>
2025-05-07 15:06:51 -0300

    Initial commit

---
+++ src/main/java/br/gov/ifgoiano/gethospeda/GetHospedaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.ifgoiano.gethospeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetHospedaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetHospedaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/gov/ifgoiano/gethospeda/GetHospedaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.ifgoiano.gethospeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetHospedaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
