7d18d443dcf569e942e2349acd326dbd6ce555c3
Cristian Martins Fernandes <cristianmarfer14@gmail.com>
2025-05-21 08:42:00 -0300

    Alterações do código para a master

---
+++ src/main/java/br/gov/ifgoiano/gethospeda/configuration/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.gov.ifgoiano.gethospeda.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API with Java and Spring Boot")
+                        .version("v1")
+                        .description("API example about Person")
+                        .termsOfService("https://ifgoiano.urt.gov/projetos")
+                        .license(
+                                new License()
+                                        .name("Apache 2.0")
+                                        .url("https://ifgoiano.urt.gov/projetos")
+                        )
+                );
+    }
+}
