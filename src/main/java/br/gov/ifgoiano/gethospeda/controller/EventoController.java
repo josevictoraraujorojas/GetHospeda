629bc1dbbbf65ab54907a05889fc369e070922a4
Cristian Martins Fernandes <cristianmarfer14@gmail.com>
2025-05-26 09:01:21 -0300

    Alterações do código para a cristian

--- src/main/java/br/gov/ifgoiano/gethospeda/controller/EventoController.java
+++ src/main/java/br/gov/ifgoiano/gethospeda/controller/EventoController.java
@@ -2,6 +2,11 @@ package br.gov.ifgoiano.gethospeda.controller;
 
 import br.gov.ifgoiano.gethospeda.model.Evento;
 import br.gov.ifgoiano.gethospeda.service.EventoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +22,81 @@ public class EventoController {
     }
 
     @GetMapping
+    @Operation(summary = "Encontrar todos os eventos", description = "Encontrar todos os eventos",
+            tags = {"Evento"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Evento.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public List<Evento> listarTodos() {
         return service.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Encontra evento específico", description = "Encontra evento específico",
+            tags = {"Evento"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Evento.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Evento> buscarPorId(@PathVariable Long id) {
         return service.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
+    @Operation(summary = "Cadastrar evento", description = "Cadastrar evento",
+            tags = {"Evento"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Evento.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public Evento salvar(@RequestBody Evento evento) {
         return service.save(evento);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar evento", description = "Atualizar evento",
+            tags = {"Evento"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Evento.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Evento> atualizar(@PathVariable Long id, @RequestBody Evento evento) {
         if (!service.findById(id).isPresent()) return ResponseEntity.notFound().build();
         evento.setId(id);
@@ -39,6 +104,21 @@ public class EventoController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletar evento", description = "Deletar evento",
+            tags = {"Evento"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Evento.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            })
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         if (!service.findById(id).isPresent()) return ResponseEntity.notFound().build();
         service.delete(id);
