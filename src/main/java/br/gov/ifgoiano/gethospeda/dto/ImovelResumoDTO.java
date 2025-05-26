60e305f1eb553348d7f12dca3d42a94ed34708db
josevictoraraujorojas <kaneki1212112121@gmail.com>
2025-05-26 13:08:50 -0300

    Imovel resumo DTO

---
+++ src/main/java/br/gov/ifgoiano/gethospeda/dto/ImovelResumoDTO.java
@@ -0,0 +1,37 @@
+package br.gov.ifgoiano.gethospeda.dto;
+
+import java.io.Serializable;
+
+public class ImovelResumoDTO implements Serializable {
+    private long id;
+    private String titulo;
+    private String descricao;
+
+    public ImovelResumoDTO() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
