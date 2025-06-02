f517ec7ad44b5e56336f0860b895e20f03d6b321
josevictoraraujorojas <kaneki1212112121@gmail.com>
2025-06-02 07:59:34 -0300

    02/6/25 07:58 | Jose Victor Araujo Rojas / mudanca na relacao entre solicitaServico e reserva

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -1,40 +1,40 @@
 CREATE TABLE area
 (
     id                 BIGINT AUTO_INCREMENT NOT NULL,
-    tipo               VARCHAR(255) NULL,
-    descricao          VARCHAR(255) NULL,
-    imovel_id          BIGINT NULL,
-    horario_disponivel VARCHAR(255) NULL,
-    regras_de_uso      VARCHAR(255) NULL,
+    tipo               VARCHAR(255)          NULL,
+    descricao          VARCHAR(255)          NULL,
+    imovel_id          BIGINT                NULL,
+    horario_disponivel VARCHAR(255)          NULL,
+    regras_de_uso      VARCHAR(255)          NULL,
     CONSTRAINT pk_area PRIMARY KEY (id)
 );
 
 CREATE TABLE avaliacao_cliente
 (
     id              BIGINT AUTO_INCREMENT NOT NULL,
-    nota            INT NOT NULL,
-    comentario      VARCHAR(255) NULL,
-    data_avaliacao DOUBLE NULL,
-    hospede_id      BIGINT NULL,
-    proprietario_id BIGINT NULL,
+    nota            INT                   NOT NULL,
+    comentario      VARCHAR(255)          NULL,
+    data_avaliacao  DOUBLE                NULL,
+    hospede_id      BIGINT                NULL,
+    proprietario_id BIGINT                NULL,
     CONSTRAINT pk_avaliacao_cliente PRIMARY KEY (id)
 );
 
 CREATE TABLE avaliacao_imovel
 (
-    id         BIGINT AUTO_INCREMENT NOT NULL,
-    nota       INT NOT NULL,
-    comentario VARCHAR(255) NULL,
-    data_avaliacao DOUBLE NULL,
-    hospede_id BIGINT NULL,
-    imovel_id  BIGINT NULL,
+    id             BIGINT AUTO_INCREMENT NOT NULL,
+    nota           INT                   NOT NULL,
+    comentario     VARCHAR(255)          NULL,
+    data_avaliacao DOUBLE                NULL,
+    hospede_id     BIGINT                NULL,
+    imovel_id      BIGINT                NULL,
     CONSTRAINT pk_avaliacao_imovel PRIMARY KEY (id)
 );
 
 CREATE TABLE cadastra_evento
 (
-    evento_id     BIGINT NOT NULL,
-    hospede_id    BIGINT NOT NULL,
+    evento_id     BIGINT   NOT NULL,
+    hospede_id    BIGINT   NOT NULL,
     data_cadastro datetime NULL,
     CONSTRAINT pk_cadastraevento PRIMARY KEY (evento_id, hospede_id)
 );
@@ -42,103 +42,103 @@ CREATE TABLE cadastra_evento
 CREATE TABLE denuncia
 (
     id               BIGINT AUTO_INCREMENT NOT NULL,
-    descricao        VARCHAR(255) NULL,
-    motivo           VARCHAR(255) NULL,
-    data_envio       datetime NULL,
-    status           SMALLINT NULL,
-    data_validacao   datetime NULL,
-    administrador_id BIGINT NULL,
-    denunciante_id   BIGINT NULL,
-    denunciado_id    BIGINT NULL,
+    descricao        VARCHAR(255)          NULL,
+    motivo           VARCHAR(255)          NULL,
+    data_envio       datetime              NULL,
+    status           SMALLINT              NULL,
+    data_validacao   datetime              NULL,
+    administrador_id BIGINT                NULL,
+    denunciante_id   BIGINT                NULL,
+    denunciado_id    BIGINT                NULL,
     CONSTRAINT pk_denuncia PRIMARY KEY (id)
 );
 
 CREATE TABLE evento
 (
     id          BIGINT AUTO_INCREMENT NOT NULL,
-    nome        VARCHAR(255) NULL,
-    local       VARCHAR(255) NULL,
-    descricao   VARCHAR(255) NULL,
-    data_inicio datetime NULL,
-    data_fim    datetime NULL,
-    capacidade  INT NOT NULL,
-    imovel_id   BIGINT NULL,
+    nome        VARCHAR(255)          NULL,
+    local       VARCHAR(255)          NULL,
+    descricao   VARCHAR(255)          NULL,
+    data_inicio datetime              NULL,
+    data_fim    datetime              NULL,
+    capacidade  INT                   NOT NULL,
+    imovel_id   BIGINT                NULL,
     CONSTRAINT pk_evento PRIMARY KEY (id)
 );
 
 CREATE TABLE imovel
 (
     id                       BIGINT AUTO_INCREMENT NOT NULL,
-    titulo                   VARCHAR(255) NULL,
-    descricao                VARCHAR(255) NULL,
-    endereco                 VARCHAR(255) NULL,
-    cidade                   VARCHAR(255) NULL,
-    estado                   VARCHAR(255) NULL,
-    cep                      VARCHAR(255) NULL,
-    preco_diaria             FLOAT NOT NULL,
-    politica_de_cancelamento VARCHAR(255) NULL,
-    status                   VARCHAR(255) NULL,
-    proprietario_id          BIGINT NULL,
+    titulo                   VARCHAR(255)          NULL,
+    descricao                VARCHAR(255)          NULL,
+    endereco                 VARCHAR(255)          NULL,
+    cidade                   VARCHAR(255)          NULL,
+    estado                   VARCHAR(255)          NULL,
+    cep                      VARCHAR(255)          NULL,
+    preco_diaria             FLOAT                 NOT NULL,
+    politica_de_cancelamento VARCHAR(255)          NULL,
+    status                   VARCHAR(255)          NULL,
+    proprietario_id          BIGINT                NULL,
     CONSTRAINT pk_imovel PRIMARY KEY (id)
 );
 
 CREATE TABLE quarto
 (
     id                 BIGINT AUTO_INCREMENT NOT NULL,
-    tipo               VARCHAR(255) NULL,
-    quantidade_camas   INT    NOT NULL,
-    banheiro_privativo BIT(1) NOT NULL,
-    aream2 DOUBLE NOT NULL,
-    descricao          VARCHAR(255) NULL,
-    imovel_id          BIGINT NULL,
+    tipo               VARCHAR(255)          NULL,
+    quantidade_camas   INT                   NOT NULL,
+    banheiro_privativo BIT(1)                NOT NULL,
+    aream2             DOUBLE                NOT NULL,
+    descricao          VARCHAR(255)          NULL,
+    imovel_id          BIGINT                NULL,
     CONSTRAINT pk_quarto PRIMARY KEY (id)
 );
 
 CREATE TABLE reserva
 (
     id          BIGINT AUTO_INCREMENT NOT NULL,
-    data_inicio datetime NULL,
-    data_fim    datetime NULL,
-    reserva     datetime NULL,
-    valor_total DOUBLE NOT NULL,
-    status      VARCHAR(255) NULL,
-    hospede_id  BIGINT NULL,
-    quarto_id   BIGINT NULL,
+    data_inicio datetime              NULL,
+    data_fim    datetime              NULL,
+    reserva     datetime              NULL,
+    valor_total DOUBLE                NOT NULL,
+    status      VARCHAR(255)          NULL,
+    hospede_id  BIGINT                NULL,
+    quarto_id   BIGINT                NULL,
     CONSTRAINT pk_reserva PRIMARY KEY (id)
 );
 
 CREATE TABLE servico
 (
     id              BIGINT AUTO_INCREMENT NOT NULL,
-    nome            VARCHAR(255) NULL,
-    descricao       VARCHAR(255) NULL,
-    preco DOUBLE NOT NULL,
-    disponibilidade BIT(1) NOT NULL,
-    imovel_id       BIGINT NULL,
+    nome            VARCHAR(255)          NULL,
+    descricao       VARCHAR(255)          NULL,
+    preco           DOUBLE                NOT NULL,
+    disponibilidade BIT(1)                NOT NULL,
+    imovel_id       BIGINT                NULL,
     CONSTRAINT pk_servico PRIMARY KEY (id)
 );
 
 CREATE TABLE solicita_servico
 (
     data_solicitacao datetime NULL,
-    reserva_id       BIGINT NOT NULL,
-    servico_id       BIGINT NOT NULL,
+    reserva_id       BIGINT   NOT NULL,
+    servico_id       BIGINT   NOT NULL,
     CONSTRAINT pk_solicitaservico PRIMARY KEY (reserva_id, servico_id)
 );
 
 CREATE TABLE usuario
 (
     id              BIGINT AUTO_INCREMENT NOT NULL,
-    dtype           VARCHAR(31) NULL,
-    nome            VARCHAR(255) NULL,
-    email           VARCHAR(255) NULL,
-    senha           VARCHAR(255) NULL,
-    telefone        VARCHAR(255) NULL,
-    tipo            SMALLINT NULL,
-    rg              VARCHAR(255) NULL,
-    data_nascimento datetime NULL,
-    matricula       VARCHAR(255) NULL,
-    cnpj            VARCHAR(255) NULL,
+    dtype           VARCHAR(31)           NULL,
+    nome            VARCHAR(255)          NULL,
+    email           VARCHAR(255)          NULL,
+    senha           VARCHAR(255)          NULL,
+    telefone        VARCHAR(255)          NULL,
+    tipo            SMALLINT              NULL,
+    rg              VARCHAR(255)          NULL,
+    data_nascimento datetime              NULL,
+    matricula       VARCHAR(255)          NULL,
+    cnpj            VARCHAR(255)          NULL,
     CONSTRAINT pk_usuario PRIMARY KEY (id)
 );
 
