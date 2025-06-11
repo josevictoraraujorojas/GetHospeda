8767495e902e5d7c2d12844e0def4c41d9eaf65a
Cristian Martins Fernandes <cristianmarfer14@gmail.com>
2025-06-11 08:39:27 -0300

    Alterações do código para a cristian

--- src/main/java/br/gov/ifgoiano/gethospeda/dto/EventoDTO.java
+++ src/main/java/br/gov/ifgoiano/gethospeda/dto/EventoDTO.java
@@ -1,5 +1,6 @@
 package br.gov.ifgoiano.gethospeda.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import org.springframework.hateoas.RepresentationModel;
 
@@ -9,27 +10,36 @@ import java.util.Objects;
 
 public class EventoDTO extends RepresentationModel<EventoDTO> implements Serializable {
 
+    @Schema(description = "Identificador único do evento", example = "1")
     private long id;
+
+    @Schema(description = "Nome do evento", example = "Festa Junina")
     @NotBlank(message = "O nome do evento é obrigatório.")
     private String nome;
 
+    @Schema(description = "Local que o evento vai ocorrer", example = "Quadra Coberta")
     @NotBlank(message = "O local do evento é obrigatório.")
     private String local;
 
+    @Schema(description = "Descrição do evento", example = "Festividade do mês de junho")
     @NotBlank(message = "A descrição do evento é obrigatória.")
     private String descricao;
 
+    @Schema(description = "Data que se inicia o evento", example = "2025-12-01T10:00:00Z")
     @NotNull(message = "A data de início é obrigatória.")
     @FutureOrPresent(message = "A data de início deve ser no presente ou no futuro.")
     private Date dataInicio;
 
+    @Schema(description = "Data que termina o evento", example = "2025-12-03T10:00:00Z")
     @NotNull(message = "A data de fim é obrigatória.")
     @Future(message = "A data de fim deve ser no futuro.")
     private Date dataFim;
 
+    @Schema(description = "Capacidade máxima do evento", example = "40")
     @Min(value = 1, message = "A capacidade mínima do evento deve ser de pelo menos 1.")
     private int capacidade;
 
+    @Schema(description = "Id do imóvel que o evento está ligado", example = "1")
     @NotNull(message = "O imóvel associado é obrigatório.")
     private ImovelResumoDTO imovel;
 
